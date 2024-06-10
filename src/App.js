541e820b3b4b07f014a55683953302207e8fb839
Juan Mateo Frasinelli <d46710280@alumnos.itr.edu.ar>
2024-06-10 11:21:42 -0300

    Se cambio la lista de Materias

--- src/App.js
+++ src/App.js
@@ -2,14 +2,58 @@ import React from 'react';
 import './App.css';
 function App() {
 
-  const subjects = ["Matemática", "Lengua", "Programación II", "Física", "Informática Aplicada II", "Química", "Robótica" ]
+  const subjects = [
+    {
+      name: "Matemática",
+      teacher: "",
+      marks: [],
+      year: NaN,
+      id: 0
+    },
+    {
+      name: "Lengua",
+      teacher: "",
+      marks: [],
+      year: NaN,
+      id: 1
+    },
+    {
+      name: "Programación II",
+      teacher: " ",
+      marks: [],
+      year: NaN,
+      id: 2
+    },
+    {
+      name: "Física",
+      teacher: " ",
+      marks: [],
+      year: NaN,
+      id: 4
+
+    },
+    {
+      name: "Informática Aplicada II",
+      teacher: " ",
+      marks: [],
+      year: NaN,
+      id: 5
+    },
+    {
+      name: "Química",
+      teacher: " ",
+      marks: [],
+      year: NaN,
+      id: 6
+    }
+  ];
 
   return (
     <div>
       <header className="App-header">
         <h3>Materias ciclo lectivo 2023</h3>
         <ul>
-          {subjects.map((subject) => <li>{subject}</li>)}
+          {subjects.map((subject) => <li key={subject.id} >{subject.name}</li>)}
         </ul>
       </header>
     </div>
